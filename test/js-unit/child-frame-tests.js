1ffb04a411cb478222d5bf174c03083fc0ea69d6
Eric Peterson <i.am@eric.pe>
2014-11-24 18:35:54 -0800

    Ensuring the framecomm initializer is chainable. [#2380123]

--- test/js-unit/child-frame-tests.js
+++ test/js-unit/child-frame-tests.js
@@ -1,14 +1,41 @@
 (function($) {
 
-  module('drupal#formframe child frame tests', {
+  module('drupal#formframe framecomm tests', {
     setup: function() {
       this.formContainer = $('#qunit-fixture');
     }
   });
 
-  test('test', function() {
-    equal(1, 1, '1 and 1 are the same.');
+  test('framecomm is chainable', function() {
+    strictEqual(this.formContainer.framecomm({}), this.formContainer, 'is chainable');
     expect(1);
+  });
+
+  test('extends default configurations', function() {
+    var oldExtend = $.extend,
+        expectedOptions = {
+          foo: 'bar',
+          baz: 'fizz'
+        },
+        expectedDefaults = $.fn.framecomm.defaults,
+        mockData = {};
+
+    // Override the core extend method.
+    $.extend = function(arg1, arg2, arg3) {
+      mockData.defaults = arg2;
+      mockData.options = arg3;
+      return oldExtend.call(this, arg1, arg2, arg3);
+    };
+
+    // Call the form comm plugin and assert the correct effects.
+    this.formContainer.framecomm(expectedOptions);
+    deepEqual(mockData.defaults, expectedDefaults, 'should be called with expected defaults');
+    deepEqual(mockData.options, expectedOptions, 'should be called with passed options');
+
+    // Reset methods.
+    $.extend = oldExtend;
+
+    expect(2);
   });
 
 }(jQuery));
